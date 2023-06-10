3c452ce1c686de32cc43f9f799e40393953cb426
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-06-10 11:23:58 -0300

    :hammer: feat: Add useForm and handleClaimUsername function to test react-hook-form

--- src/components/ClaimUsernameForm/index.tsx
+++ src/components/ClaimUsernameForm/index.tsx
@@ -6,10 +6,32 @@ import { Form } from "./styles";
 // Icons
 import { ArrowRight } from "phosphor-react";
 
+// Forms
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { create } from "domain";
+
+const createClaimUsernameFormSchema = z.object({
+  username: z.string(),
+});
+
+type ClaimUsernameForm = z.infer<typeof createClaimUsernameFormSchema>;
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameForm>();
+
+  async function handleClaimUsername(data: ClaimUsernameForm) {
+    console.log(data.username);
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register("username")}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
