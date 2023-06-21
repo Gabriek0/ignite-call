49e5ab0c6b8d02890a9f0663d71f07c56f9af952
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-06-20 21:44:30 -0300

    :hammer: feat: Create connect-calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,40 @@
+import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
+
+// Icon
+import { ArrowRight } from "phosphor-react";
+
+// Styles
+import { ConnectBox, ConnectItem } from "./styles";
+import { Container, Header } from "../styles";
+
+export default function ConnectCalendarPage() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  );
+}
---
+++ src/pages/register/connect-calendar/styles.ts
@@ -0,0 +1,20 @@
+import { Box, styled } from "@ignite-ui/react";
+
+export const ConnectBox = styled(Box, {
+  display: "flex",
+  flexDirection: "column",
+  marginTop: "$6",
+});
+
+export const ConnectItem = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+
+  padding: "$4 $6",
+
+  borderRadius: "$md",
+  border: "1px solid $gray600",
+
+  marginBottom: "$2",
+});
