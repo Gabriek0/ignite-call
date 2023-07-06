f35461bb3ebd2bdc9ecad4f9e02ad7a8e8359fd0
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-06 20:40:23 -0300

    :hammer: feat: Add getStaticProps and getStaticPaths to schedule with dynamic route page

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,63 @@
+// Styles
+import { Avatar, Heading, Text } from "@ignite-ui/react";
+import { Container, UserHeader } from "./styles";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+import { prisma } from "@/lib/prisma";
+
+type User = {
+  name: string;
+  avatar_url: string;
+  bio: string;
+};
+
+interface SchedulePageProps {
+  user: User;
+}
+
+export default function SchedulePage({ user }: SchedulePageProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatar_url} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}: GetStaticPropsContext) => {
+  const username = params?.username as string;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!username || !user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      user: {
+        avatar_url: user.avatar_url,
+        name: user.name,
+        bio: user.bio,
+      },
+    },
+    redirect: 60 * 60 * 24, // 1 day
+  };
+};
---
+++ src/pages/schedule/[username]/styles.ts
@@ -0,0 +1,24 @@
+import { Heading, Text, styled } from "@ignite-ui/react";
+
+export const Container = styled("div", {
+  // max width to calendar open
+  maxWidth: 852,
+  padding: "0 $4",
+
+  margin: "$20 auto $4",
+});
+
+export const UserHeader = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
+  [`> ${Heading}`]: {
+    marginTop: "$2",
+    lineHeight: "$base",
+  },
+
+  [`> ${Text}`]: {
+    color: "$gray200",
+  },
+});
