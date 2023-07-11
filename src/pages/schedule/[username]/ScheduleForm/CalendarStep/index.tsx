61a85e814da6273b521b5a3bb12f50b0f35a4edb
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-11 15:22:28 -0300

    :hammer: feat: Create styles for TimerPicker in CalendarStep component

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,79 @@
 import { Calendar } from "@/components/Calendar";
-import { Container } from "./styles";
+
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from "./styles";
+
+import { Text } from "@ignite-ui/react";
 
 export function CalendarStep() {
+  const isDateSelected = true;
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira, <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem disabled>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+
+            <TimePickerItem>
+              <Text>14:00</Text>
+            </TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   );
 }
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@ignite-ui/react";
+import { Box, Text, styled } from "@ignite-ui/react";
 
 export const Container = styled(Box, {
   padding: 0,
@@ -8,6 +8,83 @@ export const Container = styled(Box, {
   position: "relative",
   maxWidth: "100%",
 
-  width: 540,
-  gridTemplateColumns: "1fr",
+  variants: {
+    isTimePickerOpen: {
+      true: {
+        gridTemplateColumns: "1fr 280px",
+
+        "@media (max-width: 900px": {
+          gridTemplateColumns: "1fr",
+        },
+      },
+      false: {
+        width: 540,
+        gridTemplateColumns: "1fr",
+      },
+    },
+  },
+});
+
+export const TimePicker = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "$6",
+
+  overflowY: "scroll",
+
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  right: 0,
+
+  borderLeft: "1px solid $gray600",
+});
+
+export const TimePickerHeader = styled(Text, {
+  textAlign: "left",
+  fontWeight: 600,
+
+  marginBottom: "$3",
+
+  span: {
+    color: "$gray200",
+  },
+});
+
+export const TimePickerList = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "$2",
+
+  width: "100%",
+
+  [`${Text}`]: {
+    textAlign: "center",
+  },
+});
+
+export const TimePickerItem = styled("button", {
+  cursor: "pointer",
+
+  border: 0,
+  borderRadius: "6px",
+  background: "$gray600",
+
+  padding: "$2 0",
+
+  "&:disabled": {
+    background: "none",
+    cursor: "defualt",
+    opacity: 0.4,
+    border: "1px solid $gray500",
+  },
+
+  "&:not(:disabled):hover": {
+    background: "$gray500",
+  },
+
+  "&:focus": {
+    boxShadow: "0 0 0 2px $colors$gray100",
+  },
 });
