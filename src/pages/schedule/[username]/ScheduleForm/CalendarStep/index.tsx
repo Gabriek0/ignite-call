66a4ea3af39f4a8189bd0017361a862897b3f8e5
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-01 20:13:30 -0300

    :hammer: feat: Add useEffect to make get request to availability route

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar } from '@/components/Calendar'
+import { Calendar } from "@/components/Calendar";
 
 import {
   Container,
@@ -6,30 +6,55 @@ import {
   TimePickerHeader,
   TimePickerItem,
   TimePickerList,
-} from './styles'
+} from "./styles";
 
-import { capitalize } from '@/utils/capitalize'
-import { Text } from '@ignite-ui/react'
-import dayjs from 'dayjs'
-import { useMemo, useState } from 'react'
+import { api } from "@/lib/axios";
+import { capitalize } from "@/utils/capitalize";
+import { Text } from "@ignite-ui/react";
+import dayjs from "dayjs";
+import { useRouter } from "next/router";
+import { useEffect, useMemo, useState } from "react";
 
 export function CalendarStep() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const router = useRouter();
+
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [availability, setAvailability] = useState(null);
 
   const weekDaySelected = useMemo(() => {
     const weekDay = dayjs()
       .day(selectedDate?.getDay() || 0)
-      .format('dddd')
+      .format("dddd");
 
-    return capitalize(weekDay)
-  }, [selectedDate])
+    return capitalize(weekDay);
+  }, [selectedDate]);
 
   const weekDayExtendedSelected = useMemo(() => {
-    const day = dayjs(selectedDate).format('DD')
-    const month = dayjs(selectedDate).format('MMMM')
+    const day = dayjs(selectedDate).format("DD");
+    const month = dayjs(selectedDate).format("MMMM");
 
-    return `${day} de ${capitalize(month)}`
-  }, [selectedDate])
+    return `${day} de ${capitalize(month)}`;
+  }, [selectedDate]);
+
+  const username = router.query.username as string;
+
+  useEffect(() => {
+    if (!selectedDate) {
+      return;
+    }
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format("YYYY-MM-DD"),
+        },
+      })
+      .then((response) => {
+        const times = response.data;
+
+        console.log(times);
+      });
+  }, [selectedDate]);
 
   return (
     <Container isTimePickerOpen={!!selectedDate}>
@@ -93,5 +118,5 @@ export function CalendarStep() {
         </TimePicker>
       )}
     </Container>
-  )
+  );
 }
